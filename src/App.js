5f9244dbdae52d3b0f71f063ca1187f08b90b427
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-08-13 11:04:37 +0300

    Add states for text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./assets/styles/Style.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Editor from "./components/Editor";
 
 const App = () => {
-  return <h1>Hello</h1>;
-}
+  const [text, setText] = useState("# سلام دنیا");
+
+  return (
+    <div id="app">
+      <Header text={text} />
+      <Editor text={text} setText={setText} />
+      <Footer text={text} />
+    </div>
+  );
+};
 
 export default App;
