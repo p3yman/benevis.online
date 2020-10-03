da63ff385435d80cdae6c5cac7cd5536631b4442
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-10-03 12:00:46 +0300

    Add publicId for the preview link

--- src/pages/Viewer.js
+++ src/pages/Viewer.js
@@ -4,33 +4,38 @@ import db from "../firebase";
 import { navigate } from "@reach/router";
 
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Loading from "../components/Loading";
 
 const Viewer = ({ id }) => {
-  const [text, setText] = useState("# سلام دنیا");
+  const [document, setDocument] = useState(null);
 
   useEffect(() => {
-    const ref = db.collection("posts").where("id", "==", id);
+    const ref = db.collection("posts").where("publicId", "==", id);
 
     ref.get().then((results) => {
       if (!results.empty) {
         const data = results.docs[0].data();
-        setText(data.text);
+        console.log(data);
+        setDocument(data);
       } else {
         navigate("/404");
       }
     });
   }, [id]);
 
+  if (!document) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Header />
       <div id="viewer">
         <div id="output">
-          <Markdown source={text} />
+          <Markdown source={document.text} />
         </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
